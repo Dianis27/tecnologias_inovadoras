dc7f0d6d332e40e8d3dd6e2aca7bcf160012c9b1
Dianis27 <32026478+Dianis27@users.noreply.github.com>
2017-10-07 16:12:09 -0500

    prueba2

---
+++ refaccionaria/ventas.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+	<title>Sistema para venta de autopartes </title>
+	<link rel="stylesheet" href="estilos.css">
+	<link rel="stylesheet" href="fonts.css">
+	<style type="text/css">
+	body {
+	background-color: #E6E6E6;
+}
+    </style>
+	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+</head>
+<body>
+	<header>
+		<div class="menu_bar">
+			<a href="#" class="bt-menu"><span class="icon-menu"></span>Refaccionaria</a>
+		</div>
+
+		<nav>
+			<ul>
+				<li><a href="#"><span class="#">Refaccionaria Automotriz</span></a></li>
+				<li><a href="index.html"><span class="icon-home2"></span>Inicio</a></li>
+				<li><a href="productos.php"><span class="icon-books"></span>Productos</a></li>
+				<li><a href="empleados.php"><span class="icon-pencil"></span>Empleados</a></li>
+                <li><a href="clientes.php"><span class="icon-address-book"></span>Clientes</a></li>
+                <li><a href="proveedores.php"><span class="icon-user"></span>Proveedores</a></li>
+				<li><a href="ventas.php"><span class="icon-folder-open"></span>Ventas</a></li>
+				
+
+			</ul>
+		</nav>
+	</header>
+
+	<section>
+
+<head>
+     <meta charset="uft-8">
+     <link rel="stylesheet" href="bootstrap/css/bootstrap-css">
+     <link rel="stylesheet" href="bootstrap/css/bootstrap-responsive.css">
+     <link rel="stylesheet" type="text/css" href="estilos/estilos.css"><center><h1> Relizar Ventas.
+    
+     <p><font size="3" color="red">LLene los campos correspondientes para completar la venta </font></p>
+    
+    
+     <input type="text" name="fe" placeholder="Fecha de Venta" id="fe" oninput="validacion('fe')" required pattern="([A-Z ]{10,30})" maxlength="30"/><br>
+    
+     <input type="text" name="cod" placeholder="Empleado" id="cod" oninput="validacion('cod')" required pattern="([A-Z ]{4,20})" maxlength="20"/><br>
+    
+     <input type="text" name="co" placeholder="Codigo del Cliente" id="co" oninput="validacion('co')" required pattern="([A-Z ]{4,20})" maxlength="20"/>
+		
+     <input type="text" name="no" placeholder="Nombre Completo" id="ed" oninput="validacion('no')" required pattern="([0-9 ]{8,10})" maxlength="10"/><br>
+    
+     <input type="text" name="po" placeholder="Producto" id="po" oninput="validacion('po')" required pattern="([A-Z ]{10,30})" maxlength="30"/>
+     
+     <input type="text" name="pi" placeholder="Cantidad de Piezas" id="pi" oninput="validacion('pi')" required pattern="([A-Z ]{10,30})" maxlength="30"/><br>
+    
+    <input type="text" name="mon" placeholder="Monto" id="mon" oninput="validacion('mon')" required pattern="([A-Z ]{10,30})" maxlength="30"/><br>
+    
+    <center><input type="submit" value="Enviar"/></center>
+    
+    <center><input type="submit" value="Imprimir Factura"/></center>
+    
